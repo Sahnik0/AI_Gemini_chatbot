a51d398412bcc99ae8ff48b1c4191d2884215488
Sahnik0 <tb123983@gmail.com>
2025-03-27 00:22:05 +0530

    update 3

--- api/get.js
+++ api/get.js
@@ -2,21 +2,26 @@ import formidable from "formidable";
 
 export const config = {
     api: {
-        bodyParser: false, 
+        bodyParser: false,
     },
 };
 
-export default function handler(req, res) {
+export default async function handler(req, res) {
     if (req.method === "POST") {
-        const form = new formidable.IncomingForm();
-        form.parse(req, (err, fields, files) => {
-            if (err) {
-                res.status(500).send("Server Error");
-                return;
-            }
+        try {
+            const { fields, files } = await new Promise((resolve, reject) => {
+                const form = new formidable.IncomingForm();
+                form.parse(req, (err, fields, files) => {
+                    if (err) reject(err);
+                    else resolve({ fields, files });
+                });
+            });
             const msg = fields.msg || "";
             res.status(200).send(`Received message: ${msg}`);
-        });
+        } catch (error) {
+            console.error("Form parsing error:", error);
+            res.status(500).send("Server Error");
+        }
     } else {
         res.setHeader("Allow", ["POST"]);
         res.status(405).end(`Method ${req.method} not allowed`);
