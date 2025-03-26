9e751862de4adbc328d0e7b4a9153d80f5bec50e
Sahnik0 <tb123983@gmail.com>
2025-03-27 00:19:31 +0530

    endpoint 1

--- api/get.js
+++ api/get.js
@@ -1,8 +1,22 @@
+import formidable from "formidable";
+
+export const config = {
+    api: {
+        bodyParser: false, 
+    },
+};
+
 export default function handler(req, res) {
     if (req.method === "POST") {
-        const msg = req.body.msg || "";
-        
-        res.status(200).send(`Received message: ${msg}`);
+        const form = new formidable.IncomingForm();
+        form.parse(req, (err, fields, files) => {
+            if (err) {
+                res.status(500).send("Server Error");
+                return;
+            }
+            const msg = fields.msg || "";
+            res.status(200).send(`Received message: ${msg}`);
+        });
     } else {
         res.setHeader("Allow", ["POST"]);
         res.status(405).end(`Method ${req.method} not allowed`);
